18b8ad2825370bbc0c0fcefebbaa5187e45eb045
Adii22-22 <adinagalkar@gmail.com>
2025-11-21 16:38:07 +0530

    code uploaded

--- bit_manipulation/136_singleNumber.py
+++ bit_manipulation/136_singleNumber.py
@@ -9,3 +9,9 @@
 #
 # Time Complexity: O(n)
 # Space Complexity: O(1)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int: # type: ignore
+        x  = 0
+        for num  in nums:
+            x = x ^ num
+        return x
