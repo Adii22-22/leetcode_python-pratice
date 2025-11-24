79ccf7144eb711d14d85bc65e164c88c3f580bd9
Adii22-22 <adinagalkar@gmail.com>
2025-11-24 16:36:27 +0530

    final code updated

--- strings/412_fizz_buzz.py
+++ strings/412_fizz_buzz.py
@@ -15,3 +15,16 @@ Approach:
 Time Complexity: O(n)
 Space Complexity: O(n)
 """
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]: # type: ignore
+        arr = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                arr.append("FizzBuzz")
+            elif i % 3 == 0:
+                arr.append("Fizz")
+            elif i % 5 == 0:
+                arr.append("Buzz")
+            else:
+                arr.append(str(i))
+        return arr
