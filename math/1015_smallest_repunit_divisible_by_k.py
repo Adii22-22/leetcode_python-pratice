fc9763ff8cb7648c428cac6bc78247be123246cb
Adii22-22 <adinagalkar@gmail.com>
2025-11-25 23:53:46 +0530

    code uploaded

--- math/1015_smallest_repunit_divisible_by_k.py
+++ math/1015_smallest_repunit_divisible_by_k.py
@@ -17,3 +17,15 @@
 #
 # Time Complexity: O(k)
 # Space Complexity: O(1)
+class Solution:
+    def smallestRepunitDivByK(self, k: int) -> int:
+        if k % 2 == 0 or k % 5 ==0:
+            return -1
+        reminder = 0
+        length = 0
+        for i in range(1,k+1):
+            reminder = (reminder * 10 + 1) % k
+            if reminder == 0:
+                return i
+        return -1
+        
