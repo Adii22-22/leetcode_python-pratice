3fc96889a30b8936d32cabdd98e5ba726e2f9309
Adii22-22 <adinagalkar@gmail.com>
2025-11-27 22:46:22 +0530

    uploaded code

--- math/66_plus_one.py
+++ math/66_plus_one.py
@@ -16,3 +16,13 @@
 # Time Complexity: O(n)
 # Space Complexity: O(1) (modifies list in place)
 # --------------------------------------------------------------
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]: # type: ignore
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i]< 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+        return [1] + digits
+         
