ce695d37e116adce1e48c0a710740cbebff9be69
Adii22-22 <adinagalkar@gmail.com>
2025-11-29 23:54:45 +0530

    code uploaded

--- arrays/118_pascals_triangle.py
+++ arrays/118_pascals_triangle.py
@@ -7,3 +7,23 @@
 #   the row with 1. Repeat this process for numRows rows.
 # Time Complexity: O(n^2)
 # Space Complexity: O(n^2)
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]: # type: ignore
+        triangle = []
+
+        for _ in range(numRows):
+            # start each row with 1
+            row = [1]
+
+            # if not the first row, fill middle elements
+            if triangle:
+                prev = triangle[-1]
+                for i in range(1, len(prev)):
+                    row.append(prev[i - 1] + prev[i])
+
+                # end each row with 1
+                row.append(1)
+
+            triangle.append(row)
+
+        return triangle
