64b600500a1ab487a12953a7dfa2f188244b5f99
Adii22-22 <adinagalkar@gmail.com>
2025-11-30 23:08:21 +0530

    code uploaded

--- math/258_add_digits.py
+++ math/258_add_digits.py
@@ -6,3 +6,8 @@
 #   1 + (num - 1) % 9. Zero returns 0. Avoids loops and repeated summing.
 # Time Complexity: O(1)
 # Space Complexity: O(1)
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        return 1 + (num - 1) % 9
