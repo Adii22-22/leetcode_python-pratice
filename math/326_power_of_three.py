7f55a8cdf869aa712330764c56093d3232546bf4
Adii22-22 <adinagalkar@gmail.com>
2025-12-02 21:53:24 +0530

    comments added

---
+++ math/326_power_of_three.py
@@ -0,0 +1,9 @@
+# LeetCode 326: Power of Three
+# Date solved: Dec 2, 2025
+# Approach: Math / Repeated Division
+# Explanation:
+#   A number is a power of three if it can be repeatedly divided by 3 until
+#   it becomes 1. First, discard non-positive values. Then divide n by 3
+#   while divisible. After the loop, if the result is 1, it was a power of 3.
+# Time Complexity: O(log₃ n)
+# Space Complexity: O(1)
