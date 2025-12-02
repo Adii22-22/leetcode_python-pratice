5f82585b6ced54ed57bbe70806d548a9627a6a0e
Adii22-22 <adinagalkar@gmail.com>
2025-12-02 21:55:46 +0530

    code uploaded

--- math/326_power_of_three.py
+++ math/326_power_of_three.py
@@ -7,3 +7,14 @@
 #   while divisible. After the loop, if the result is 1, it was a power of 3.
 # Time Complexity: O(log₃ n)
 # Space Complexity: O(1)
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        
+        while n % 3 == 0:
+            n = n//3
+        if n == 1:
+                return True
+        else:
+                return False
