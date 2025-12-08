373a8c54266428a36408c8e53557e88c8390de08
Adii22-22 <adinagalkar@gmail.com>
2025-12-08 17:37:40 +0530

    code uploaded

--- binary_search/704_binary_search.py
+++ binary_search/704_binary_search.py
@@ -7,3 +7,18 @@
 #   Returns index if found, otherwise returns -1.
 # Time Complexity: O(log n)
 # Space Complexity: O(1)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int: # type: ignore
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left+right)//2
+
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+        return -1
