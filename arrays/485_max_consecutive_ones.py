cc627cc1f918358fa90140aecd137e8d6580d715
Adii22-22 <adinagalkar@gmail.com>
2025-12-08 17:41:49 +0530

    feat: code uploaded

--- arrays/485_max_consecutive_ones.py
+++ arrays/485_max_consecutive_ones.py
@@ -6,3 +6,15 @@
 #   Reset the count when encountering a 0 and track the maximum streak.
 # Time Complexity: O(n)
 # Space Complexity: O(1)
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int: # type: ignore
+        count = 0
+        max_count = 0
+
+        for num in nums:
+            if num == 1:
+                count += 1
+                max_count = max(max_count,count)
+            else:
+                count = 0
+        return max_count
