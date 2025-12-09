f6e60ffcb66bb66aba9923fe98b5b8bef882e518
Adii22-22 <adinagalkar@gmail.com>
2025-12-09 16:47:15 +0530

    code uploaded

--- arrays/1365_smaller_numbers_than_current.py
+++ arrays/1365_smaller_numbers_than_current.py
@@ -8,3 +8,14 @@
 #   Build the result by mapping each number in the original array to its stored count.
 # Time Complexity: O(n log n)  # sorting dominates
 # Space Complexity: O(n)       # dictionary + result list
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]: # type: ignore
+        new_nums = sorted(nums)
+        result = []
+        small_value = {}
+        for i, num in enumerate(new_nums):
+            if num not in small_value:
+                small_value[num] = i
+        for num in nums:
+            result.append(small_value[num])
+        return result
