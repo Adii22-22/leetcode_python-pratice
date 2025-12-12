b2e0444df428e3b6fc53fde005f1a57754986c94
Adii22-22 <adinagalkar@gmail.com>
2025-12-12 18:40:42 +0530

    code uploaded

--- hashing/1189_maximum_balloons.py
+++ hashing/1189_maximum_balloons.py
@@ -8,3 +8,18 @@
 #   - Divide counts of 'l' and 'o' by 2 because they're used twice.
 #   - The minimum count among required characters decides the answer.
 # ----------------------------------------------------------
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        freq = defaultdict(int) # type: ignore
+
+        # Count only relevant letters
+        for ch in text:
+            if ch in "balloon":
+                freq[ch] += 1
+
+        # 'l' and 'o' appear twice in "balloon"
+        l = freq['l'] // 2
+        o = freq['o'] // 2
+
+        # Return minimum available complete sets
+        return min(freq['b'], freq['a'], l, o, freq['n'])
