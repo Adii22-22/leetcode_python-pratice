7a4965d990b3bb9fee655726f1aa4b68f72f5658
Adii22-22 <adinagalkar@gmail.com>
2025-12-12 18:42:46 +0530

    code uploaded

--- hashing/219_contains_duplicate_II.py
+++ hashing/219_contains_duplicate_II.py
@@ -8,3 +8,21 @@
 #   - If the same number appears again and the index 
 #     difference is <= k → duplicates are "nearby".
 # ----------------------------------------------------------
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool: # type: ignore
+        last_seen = {}
+
+        for i in range(len(nums)):
+            num = nums[i]
+
+            # If we've seen this number before
+            if num in last_seen:
+                # Check distance between current and previous index
+                if i - last_seen[num] <= k:
+                    return True
+
+            # Update last seen index
+            last_seen[num] = i
+
+        return False
