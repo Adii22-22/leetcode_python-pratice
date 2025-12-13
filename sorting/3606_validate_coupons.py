7fb2b8f673871b968818a52f36fab7cb450d5d0c
Adii22-22 <adinagalkar@gmail.com>
2025-12-13 21:56:29 +0530

    code uploaded

--- sorting/3606_validate_coupons.py
+++ sorting/3606_validate_coupons.py
@@ -18,3 +18,19 @@ O(n log n) due to sorting.
 Space Complexity:
 O(n) for storing valid coupons.
 """
+class Solution:
+    def validateCoupons(self, code: List[str], businessLine: List[str], isActive: List[bool]) -> List[str]: # type: ignore
+        allowed_coupons = []
+        priority = dict({"electronics":0, "grocery":1, "pharmacy":2, "restaurant":3})
+        for i,j,k in zip(code,businessLine,isActive):
+            valid_code = all(c.isalnum() or c == "_" for c in i)
+            if valid_code and  (len(i)> 0):
+                if j in priority:
+                    if k == True:
+                        allowed_coupons.append([j,i])
+
+        sorted_list = sorted(
+            allowed_coupons,
+            key=lambda x: (priority[x[0]], x[1])
+        )
+        return [x[1] for x in sorted_list]
