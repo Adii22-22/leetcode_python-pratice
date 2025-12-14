c406250b913bfa410eaa29558cb8b8c65ee2bc3e
Adii22-22 <adinagalkar@gmail.com>
2025-12-14 16:36:08 +0530

    code uploaded

--- sliding_window/438_find_all_anagrams_in_a_string.py
+++ sliding_window/438_find_all_anagrams_in_a_string.py
@@ -18,3 +18,27 @@
 # Time Complexity: O(n)
 # Space Complexity: O(1)
 # -------------------------------------------------------------
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]: # type: ignore
+        need = defaultdict(int) # type: ignore
+        window = defaultdict(int) # type: ignore
+        window_lenth = len(p)
+        res = []
+        l = 0
+
+        for char in p:
+            need[char] += 1
+
+        for r in range(0,len(s)):
+            window[s[r]] +=1
+
+            if r-l + 1 > len(p):
+                left_char = s[l]
+                window[left_char] -= 1
+                if window[left_char] == 0:
+                    del window[left_char]
+                l +=1
+
+            if window == need:
+                res.append(l)
+        return res
