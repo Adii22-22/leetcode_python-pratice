4d1ebc51c7dc7358eb9367cbd7fa1fd13bb3cddc
Adii22-22 <adinagalkar@gmail.com>
2025-12-16 23:45:26 +0530

    code uploaded

--- strings/longest_palindrome_409.py
+++ strings/longest_palindrome_409.py
@@ -10,3 +10,22 @@ Approach:
 Time Complexity: O(n)
 Space Complexity: O(1)  # fixed alphabet
 """
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        frequency = defaultdict(int) # type: ignore
+        for char in s:
+            frequency[char]+= 1
+
+        length = 0
+        odd_found = False
+        for count in frequency.values():
+            if count % 2 ==0:
+                length += count
+            else:
+                length += count - 1
+                odd_found = True
+            
+        if odd_found:
+            length +=1
+
+        return length
