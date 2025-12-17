6bd3dea2ccdaf88eb18a46e9e782d011c14fda84
Adii22-22 <adinagalkar@gmail.com>
2025-12-17 23:20:12 +0530

    code uploaded

--- stack/496_next_greater_element_I.py
+++ stack/496_next_greater_element_I.py
@@ -21,3 +21,19 @@ Space Complexity: O(n)
 
 Status: Accepted ✅
 """
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]: # type: ignore
+        next_greater = {}
+        stack = []
+        n = len(nums2)
+
+        for i in range(n):
+            while stack and nums2[i] > nums2[stack[-1]]:
+                index = stack.pop()
+                next_greater[nums2[index]] = nums2[i]      
+            stack.append(i)
+        
+        for i in stack:
+            next_greater[nums2[i]] = -1
+
+        return [next_greater[x] for x in nums1]
