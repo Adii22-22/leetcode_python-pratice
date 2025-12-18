5d6bd02bb146413b827113e3616e180896a0468e
Adii22-22 <adinagalkar@gmail.com>
2025-12-18 23:55:49 +0530

    code uploaded

--- arrays/228_Summary_range.py
+++ arrays/228_Summary_range.py
@@ -8,3 +8,24 @@
 #
 # Time Complexity: O(n)
 # Space Complexity: O(1) (excluding output list)
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]: # type: ignore
+        range_list = []
+        if not nums:
+            return range_list
+        start = nums[0]
+        for i in range(1,len(nums)):
+            if nums[i] != nums[i-1] +1:
+                if start == nums[i-1]:
+                    range_list.append(f"{start}")
+                else:
+                    number = f"{start}->{nums[i-1]}"
+                    range_list.append(number)
+                start = nums[i]
+        
+        if start ==  nums[-1]:
+            range_list.append(f"{start}")
+        else:
+            number = f"{start}->{nums[-1]}"
+            range_list.append(number)
+        return range_list
