72a0a7e01c7f1f021786575d39669344ba646159
Adii22-22 <adinagalkar@gmail.com>
2025-12-23 21:47:35 +0530

    code uploaded

--- math/168_excel_sheet_column_title.py
+++ math/168_excel_sheet_column_title.py
@@ -9,3 +9,13 @@
 #
 # Time Complexity: O(log n)
 # Space Complexity: O(1)
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        result = ""
+
+        while columnNumber > 0:
+            columnNumber -= 1
+            result = chr(columnNumber % 26 + ord('A')) + result
+            columnNumber //= 26
+
+        return result
