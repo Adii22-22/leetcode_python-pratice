449594e8353c396ad02b0d5aa2c0d3ac4b9407b3
Adii22-22 <adinagalkar@gmail.com>
2025-12-24 16:14:22 +0530

    code added

--- hashing/389_find_the_difference.py
+++ hashing/389_find_the_difference.py
@@ -9,3 +9,16 @@
 #
 # Time Complexity: O(n)
 # Space Complexity: O(1)
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        t_dict = defaultdict(int) # type: ignore
+        for i in t:
+            t_dict[i] += 1
+
+        for i in s:
+            if i in t_dict:
+                t_dict[i] -= 1
+
+        for char in t_dict:
+            if t_dict[char] == 1:
+                return char
