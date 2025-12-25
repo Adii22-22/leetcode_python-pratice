6be967b747d147b0958b3177720157a3c18ac650
Adii22-22 <adinagalkar@gmail.com>
2025-12-25 13:30:31 +0530

    code uploaded

--- binary_search/852_peak_index_in_a_mountain_array.py
+++ binary_search/852_peak_index_in_a_mountain_array.py
@@ -10,3 +10,14 @@
 #
 # Time Complexity: O(log n)
 # Space Complexity: O(1)
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int: # type: ignore
+        left = 0
+        right = len(arr) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if arr[mid] < arr[mid+1]:
+                left = mid + 1
+            else:
+                right = mid
+        return left
