39edd9985f4be6fff827268040164a162d25d0d4
Adii22-22 <adinagalkar@gmail.com>
2025-12-25 12:47:01 +0530

    code uploaded

--- binary_search/153_find_minimum_in_rotated_sorted_array.py
+++ binary_search/153_find_minimum_in_rotated_sorted_array.py
@@ -11,3 +11,14 @@
 #
 # Time Complexity: O(log n)
 # Space Complexity: O(1)
+class Solution:
+    def findMin(self, nums: List[int]) -> int: # type: ignore
+        left = 0
+        right = len(nums) -1
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+        return nums[left]
