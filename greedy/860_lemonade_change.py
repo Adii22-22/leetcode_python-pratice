01bb8f678c020ff884e25c0c7e1f1ede0e8a77a9
Adii22-22 <adinagalkar@gmail.com>
2025-12-26 15:18:45 +0530

    code uploaded

--- greedy/860_lemonade_change.py
+++ greedy/860_lemonade_change.py
@@ -12,3 +12,26 @@
 #
 # Time Complexity: O(n)
 # Space Complexity: O(1)
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool: # type: ignore
+        bill_count_5 = 0
+        bill_count_10 = 0
+        for bill in bills:
+            if bill == 5:  # for $5 bills no change needed
+                bill_count_5 +=1
+            elif bill == 10: # for $10 bills one $5 change needed
+                if bill_count_5 >= 1:
+                    bill_count_5 -= 1
+                    bill_count_10 += 1
+                else:
+                    return False
+            elif bill == 20: # for $20 bills prefer change  one $5 bill and $10 bill else  three $5 bills 
+                if bill_count_5 >= 1 and bill_count_10 >= 1:
+                    bill_count_5 -= 1
+                    bill_count_10 -= 1
+                else:
+                    if bill_count_5 >=3:
+                        bill_count_5 -= 3
+                    else:
+                        return False
+        return True
