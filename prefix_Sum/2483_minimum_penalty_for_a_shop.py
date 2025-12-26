62cf3bab3cf8fe6a3c09ab7ca191504a58772714
Adii22-22 <adinagalkar@gmail.com>
2025-12-26 20:34:51 +0530

    code uploaded

--- prefix_Sum/2483_minimum_penalty_for_a_shop.py
+++ prefix_Sum/2483_minimum_penalty_for_a_shop.py
@@ -11,3 +11,19 @@
 #
 # Time Complexity: O(n)
 # Space Complexity: O(1)
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        
+        penalty = customers.count("Y")
+        min_penalty =  penalty
+        best_time = 0
+
+        for i in range(len(customers)):
+            if customers[i] == "Y":
+                penalty -= 1
+            else:
+                penalty += 1
+            if penalty < min_penalty:
+                min_penalty = penalty
+                best_time = i+1
+        return best_time
