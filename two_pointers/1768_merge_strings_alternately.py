05a0a7fb60b89537c5695dd55c00f6f22b885964
Adii22-22 <adinagalkar@gmail.com>
2025-12-27 20:33:02 +0530

    code uploaded

--- two_pointers/1768_merge_strings_alternately.py
+++ two_pointers/1768_merge_strings_alternately.py
@@ -9,3 +9,19 @@
 #
 # Time Complexity: O(n + m)
 # Space Complexity: O(n + m)
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merge_string = []
+        p1 = 0
+        p2 = 0
+        while p1 <len(word1) and p2 < len(word2):
+                merge_string.append(word1[p1])
+                merge_string.append(word2[p2])
+                p1+=1
+                p2+=1  
+        if p1 < len(word1):
+            merge_string.append(word1[p1:]) 
+        elif p2 < len(word2):
+            merge_string.append(word2[p2:])
+
+        return "".join(merge_string) 
