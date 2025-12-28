3bf76b682134ccaebfb0b0589a1858297896fa85
Adii22-22 <adinagalkar@gmail.com>
2025-12-28 16:52:01 +0530

    code uploaded

--- stack/844_backspace_string_compare.py
+++ stack/844_backspace_string_compare.py
@@ -10,3 +10,28 @@
 #
 # Time Complexity: O(n + m)
 # Space Complexity: O(n + m)
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        p1 = 0
+        p2 = 0
+        stack1 = []
+        stack2 = []
+
+        for p1 in s:
+            if p1 == "#":
+                if stack1:
+                    stack1.pop()
+            else:
+                stack1.append(p1)
+
+        for p2 in t:
+            if p2 == "#":
+                if stack2:
+                    stack2.pop()
+            else:
+                stack2.append(p2)
+ 
+        if stack1 == stack2:
+            return True
+        else:
+            return False
