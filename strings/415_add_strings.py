7cb58368d83d3fa5b16985c108aa91d0fe88796e
Adii22-22 <adinagalkar@gmail.com>
2025-12-28 15:53:53 +0530

    changes done

--- strings/415_add_strings.py
+++ strings/415_add_strings.py
@@ -10,3 +10,19 @@
 #
 # Time Complexity: O(n + m)
 # Space Complexity: O(n + m)
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        p1 = len(num1) -1
+        p2 = len(num2) -1
+        carry = 0
+        result = []
+        while p1>=0 or p2>=0  or carry >0:
+            digit1 = int(num1[p1]) if p1>=0 else 0
+            digit2 = int(num2[p2]) if p2>=0 else 0
+            total = digit1 + digit2 + carry
+            result.append(str(total%10))
+            carry = (total//10)
+            p1-=1
+            p2-=1
+        result.reverse()
+        return "".join(result)
