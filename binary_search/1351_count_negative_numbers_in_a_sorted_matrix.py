891fc5710f0d6ba2b8c3859e124cb46647282774
Adii22-22 <adinagalkar@gmail.com>
2025-12-29 21:08:25 +0530

    code uploaded

--- binary_search/1351_count_negative_numbers_in_a_sorted_matrix.py
+++ binary_search/1351_count_negative_numbers_in_a_sorted_matrix.py
@@ -9,3 +9,18 @@
 #
 # Time Complexity: O(m * log n)
 # Space Complexity: O(1)
+from typing import List
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        negative_count = 0
+        for row in range(0,len(grid)):
+            l = 0
+            r = len(grid[row]) -1
+            while l<=r:
+                mid = l + (r-l)//2
+                if grid[row][mid] < 0:
+                    r = mid-1
+                else:
+                    l = mid+1
+            negative_count += len(grid[row]) - l
+        return negative_count
