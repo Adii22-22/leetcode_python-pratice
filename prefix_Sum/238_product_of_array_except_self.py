2239421d4f373aa6cb27f894bdb8bfd991b9b5a9
Adii22-22 <adinagalkar@gmail.com>
2025-12-30 13:57:29 +0530

    code uploaded

--- prefix_Sum/238_product_of_array_except_self.py
+++ prefix_Sum/238_product_of_array_except_self.py
@@ -9,3 +9,17 @@
 #
 # Time Complexity: O(n)
 # Space Complexity: O(1) extra space (output array excluded)
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]: # type: ignore
+        n = len(nums)
+        answer = [1] * n
+        prefix_product = 1
+        suffix_product = 1
+        for i in range(n):
+            answer[i] = prefix_product 
+            prefix_product *= nums[i]
+
+        for i in range(n-1,-1,-1):
+            answer[i] *= suffix_product
+            suffix_product *= nums[i]
+        return answer
