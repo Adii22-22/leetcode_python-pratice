fc712dbca0d8d2b520c95643c75a5e267814251c
Adii22-22 <adinagalkar@gmail.com>
2025-12-30 14:26:43 +0530

    code uploaded

--- two_pointers/11_container_with_most_water.py
+++ two_pointers/11_container_with_most_water.py
@@ -10,3 +10,19 @@
 #
 # Time Complexity: O(n)
 # Space Complexity: O(1)
+class Solution:
+    def maxArea(self, height: List[int]) -> int: # type: ignore
+        n = len(height)
+        left = 0
+        right = n-1
+        max_area = 0
+        while left < right:
+            h = min(height[left],height[right])
+            width = right -left
+            area = h * width 
+            max_area = max(max_area, area)
+            if height[left] < height[right]:
+                left+=1
+            else:
+                right-=1
+        return max_area
