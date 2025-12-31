444c856ca97e1925cbb9685dab595f5e3e55633c
Adii22-22 <adinagalkar@gmail.com>
2025-12-31 18:35:00 +0530

    code uploaded

--- hashing/347_top_k_frequent_elements.py
+++ hashing/347_top_k_frequent_elements.py
@@ -9,3 +9,20 @@
 #
 # Time Complexity: O(n log n) due to sorting
 # Space Complexity: O(n) for the frequency map
+import operator
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]: # type: ignore
+        count_map = {}
+        for num in nums:
+            if num in count_map:
+                count_map[num] += 1
+            else:
+                count_map[num] = 1
+        sorted_map = sorted(count_map.items(),key=operator.itemgetter(1),reverse=True)
+
+        result = []
+        for number,freq in sorted_map:
+            if len(result) >= k:
+                break
+            result.append(number)
+        return result
