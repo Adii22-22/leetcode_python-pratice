d139fc4877dcefcc1298cae1760e7d5fcc1ca203
Adii22-22 <adinagalkar@gmail.com>
2026-01-04 22:49:04 +0530

    code uploaded

--- binary_search/240_search_a_2d_matrix_ii.py
+++ binary_search/240_search_a_2d_matrix_ii.py
@@ -12,3 +12,16 @@
 #
 # Time Complexity: O(m + n)
 # Space Complexity: O(1)
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool: # type: ignore
+        row = 0
+        col = len(matrix[row])-1
+
+        while row < len(matrix) and col >=0:
+            if matrix[row][col] == target:
+                return True
+            if matrix[row][col] > target:
+                col -=1
+            else:
+                row +=1
+        return False
