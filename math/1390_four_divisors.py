c7b60b933ed984c918434a99c0dd96d62fd9b967
Adii22-22 <adinagalkar@gmail.com>
2026-01-04 21:20:59 +0530

    code uploaded

--- math/1390_four_divisors.py
+++ math/1390_four_divisors.py
@@ -12,3 +12,29 @@
 #
 # Time Complexity: O(sum of sqrt(num)) over all numbers
 # Space Complexity: O(1)
+import math
+class Solution:
+    def sumFourDivisors(self, nums: List[int]) -> int: # type: ignore
+        total_sum = 0
+        for num in nums:
+            divisors_count = 0
+            divisors_sum = 0
+
+            for d in range(1,int(math.sqrt(num)+1)):
+                if num%d == 0:
+                    other = num//d
+
+                    if d == other:
+                        divisors_count +=1
+                        divisors_sum += other
+                    else:
+                        divisors_count+=2
+                        divisors_sum += d+other
+                    
+                    if divisors_count > 4:
+                        break
+
+            if divisors_count ==4:
+                total_sum += divisors_sum
+
+        return total_sum
