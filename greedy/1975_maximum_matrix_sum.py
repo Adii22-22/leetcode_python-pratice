b4147d6df5f29df99abb5d7b80a260594e63a551
Adii22-22 <adinagalkar@gmail.com>
2026-01-05 20:42:46 +0530

    code uploaded

--- greedy/1975_maximum_matrix_sum.py
+++ greedy/1975_maximum_matrix_sum.py
@@ -18,3 +18,24 @@
 #
 # Time Complexity: O(m * n)
 # Space Complexity: O(1)
+class Solution:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int: # type: ignore
+        abs_sum = 0
+        negative_count = 0
+        min_abs = float('inf')
+        matrix_sum = 0
+
+        for row in range(len(matrix)):
+            for col in range(len(matrix[row])):
+                if matrix[row][col] < 0:
+                    negative_count +=1
+                absolute_value = abs(matrix[row][col])  
+                abs_sum += absolute_value   
+                min_abs = min(min_abs,absolute_value)
+
+        if negative_count % 2 == 0:
+            matrix_sum += abs_sum
+            return matrix_sum
+        else:
+            matrix_sum += abs_sum - (2 * min_abs)
+            return matrix_sum
