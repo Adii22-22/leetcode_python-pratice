35ae85889e39804c79cc9d41c768f0cfbe72427c
Adii22-22 <adinagalkar@gmail.com>
2026-01-06 00:54:29 +0530

    code added

--- greedy/763_partition_labels.py
+++ greedy/763_partition_labels.py
@@ -16,3 +16,19 @@
 #
 # Time Complexity: O(n)
 # Space Complexity: O(1)  (bounded by fixed alphabet size)
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]: # type: ignore
+        last_occurance = {}
+        end = 0
+        start = 0
+        cut = []
+        for i,ch in enumerate(s):
+                last_occurance[ch] = i
+
+        for i in range(len(s)):   
+            end = max(end,last_occurance.get(s[i])) 
+            if i == end:
+                length = i - start + 1
+                cut.append(length)
+                start = i+1
+        return cut
