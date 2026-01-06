e36c2e997469ff340d7557c0f8293244bbec8c5e
Adii22-22 <adinagalkar@gmail.com>
2026-01-06 22:19:08 +0530

    code uploaded

--- tree/1161_maximum_level_sum_of_a_binary_tree.py
+++ tree/1161_maximum_level_sum_of_a_binary_tree.py
@@ -16,3 +16,41 @@
 #
 # Time Complexity: O(n), where n is the number of nodes in the tree
 # Space Complexity: O(n), due to the queue used for BFS
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:      # type: ignore
+        level = 1
+        max_sum = float('-inf')
+        queue = deque([root])
+        best_level = 1
+
+        if not root:
+            return 
+        
+        while queue:
+            size = len(queue)
+            level_sum = 0
+            for i in range(size): 
+                current_node = queue.popleft()
+
+                level_sum += current_node.val
+                if current_node.left:
+                    queue.append(current_node.left)
+                if current_node.right:
+                    queue.append(current_node.right)
+            if level_sum > max_sum:
+                max_sum = max(max_sum,level_sum)
+                best_level = level
+            level +=1
+
+        return best_level
+
+
+        
