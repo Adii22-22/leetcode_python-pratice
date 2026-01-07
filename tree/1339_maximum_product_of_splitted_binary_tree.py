f47e2fd7bf0ad10855b4dcd49f74432a68b5c2f6
Adii22-22 <adinagalkar@gmail.com>
2026-01-07 20:13:57 +0530

    code uploaded

--- tree/1339_maximum_product_of_splitted_binary_tree.py
+++ tree/1339_maximum_product_of_splitted_binary_tree.py
@@ -18,3 +18,38 @@
 #
 # Time Complexity: O(n), where n is the number of nodes in the tree
 # Space Complexity: O(h), where h is the height of the tree (recursion stack)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxProduct(self, root: Optional[TreeNode]) -> int: # type: ignore
+        total_sum = 0
+        max_product = 0
+        MOD = 10**9 + 7
+        def compute_total(node):
+            if not node:
+                return 0
+            return node.val + compute_total(node.left)+compute_total(node.right)
+        
+        total_sum = compute_total(root)
+        def dfs(node):
+            nonlocal max_product
+            if not node:
+                return 0
+
+            left_sum = dfs(node.left)
+            right_sum = dfs(node.right)
+
+            subtree_sum = node.val+ left_sum + right_sum
+
+            product = subtree_sum * (total_sum - subtree_sum)
+
+            max_product = max(max_product, product)
+            return subtree_sum
+        
+        dfs(root)
+        return max_product % MOD
