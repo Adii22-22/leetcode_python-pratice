2176b16e1e0783012490ad5af3cd97e9bf4a136c
Adii22-22 <adinagalkar@gmail.com>
2026-01-09 13:04:02 +0530

    code uploaded

--- tree/865_smallest_subtree_with_all_the_deepest_nodes.py
+++ tree/865_smallest_subtree_with_all_the_deepest_nodes.py
@@ -18,3 +18,28 @@
 #
 # Time Complexity: O(n), where n is the number of nodes in the tree
 # Space Complexity: O(h), where h is the height of the tree (recursion stack)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]: # type: ignore
+
+        def dfs(node):
+            if not node:
+                return (0,None)
+
+            (left_dept,left_node) = dfs(node.left)
+            (right_dept,right_node) = dfs(node.right)
+
+            if left_dept > right_dept:
+                return (left_dept+1,left_node)
+            elif right_dept > left_dept:
+                return (right_dept+1,right_node)
+            else:
+                return (left_dept+1,node)
+
+        return dfs(root)[1]
