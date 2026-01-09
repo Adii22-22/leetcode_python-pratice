68b549cef9d87924aa1a42e461d183aa0555e55b
Adii22-22 <adinagalkar@gmail.com>
2026-01-09 20:43:56 +0530

    code added

--- binary_search/162_find_peak_element.py
+++ binary_search/162_find_peak_element.py
@@ -18,3 +18,14 @@
 #
 # Time Complexity: O(log n)
 # Space Complexity: O(1)
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int: # type: ignore
+        left = 0
+        right = len(nums) -1
+        while left < right:
+            mid = left+ (right-left)// 2
+            if nums[mid] < nums[mid+1]:
+                left = mid+1
+            else:
+                right = mid
+        return left
