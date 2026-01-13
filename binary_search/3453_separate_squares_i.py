1902753119dc4502d2adf1e3de4d26f9a0291389
Adii22-22 <adinagalkar@gmail.com>
2026-01-13 14:11:12 +0530

    code uploaded

--- binary_search/3453_separate_squares_i.py
+++ binary_search/3453_separate_squares_i.py
@@ -29,3 +29,38 @@
 #
 # Time Complexity: O(n * log R), where n is number of squares and R is the y-range
 # Space Complexity: O(1)
+class Solution:
+    def separateSquares(self, squares: List[List[int]]) -> float: # type: ignore
+        low = min(y for _,y,_ in squares)
+        high  = max(y+l for _,y,l in squares)
+        
+        def area_diff(h):
+            below = 0
+            above = 0
+            for square  in squares:
+                bottom = square[1]
+                top  = square[1]+square[2]
+                area = square[2] * square[2]
+
+                if h <= bottom:
+                    above+=area
+                elif h >=top:
+                    below+=area
+                else:
+                    height_below = h - bottom
+                    height_above = top - h
+
+                    below += square[2] * height_below
+                    above += square[2] * height_above
+            return below - above
+        
+        while (high-low) > 1e-6:
+            mid = (high+low) / 2
+
+            diff = area_diff(mid)
+
+            if diff < 0:
+                low = mid
+            else:
+                high = mid
+        return low
