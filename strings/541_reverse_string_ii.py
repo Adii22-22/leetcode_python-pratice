b22d94d68f1df7aed9da8d4fcb897ccd68eb4301
Adii22-22 <adinagalkar@gmail.com>
2026-01-16 21:17:12 +0530

    code uploaded

--- strings/541_reverse_string_ii.py
+++ strings/541_reverse_string_ii.py
@@ -15,3 +15,17 @@
 #
 # Time Complexity: O(n), where n is the length of the string
 # Space Complexity: O(n), due to conversion of string to list
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        l = list(s)
+
+        for i in range(0,len(l),2*k):
+            left = i
+            right = min(i+k-1,len(l)-1)
+
+            while left < right:
+                l[left],l[right] = l[right],l[left]
+                left +=1
+                right -= 1
+
+        return "".join(l)
