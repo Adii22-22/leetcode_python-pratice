ef3045b7269ca7089b33f59a3bd18814fb4680a5
Adii22-22 <adinagalkar@gmail.com>
2026-01-16 20:10:29 +0530

    code uploaded

--- math/2975_maximum_square_area_by_removing_fences.py
+++ math/2975_maximum_square_area_by_removing_fences.py
@@ -18,3 +18,23 @@
 #
 # Time Complexity: O(h^2 + v^2)
 # Space Complexity: O(h^2)
+class Solution:
+    def maximizeSquareArea(self, m: int, n: int, hFences: List[int], vFences: List[int]) -> int: # type: ignore
+        mod = 10**9 + 7
+        h = [1] + sorted(hFences) + [m]
+        v = [1] + sorted(vFences) + [n]
+
+        hs = set()
+        for i in range(len(h)):
+            for j in range(i+1,len(h)):
+                hs.add(h[j]-h[i])
+        ans = -1  
+        for i in range(len(v)):
+            for j in range(i+1,len(v)):
+                d = v[j] - v[i]
+                if d in hs:
+                    ans = max(ans,d)
+        
+        if ans == -1:
+            return -1
+        return (ans*ans) % mod
