d62dd643866cc805e8352b70a1145502c7c44bc0
Adii22-22 <adinagalkar@gmail.com>
2026-01-17 19:49:05 +0530

    code uploaded

--- math/3047_largest_square_area_between_rectangles.py
+++ math/3047_largest_square_area_between_rectangles.py
@@ -20,3 +20,13 @@
 #
 # Time Complexity: O(n^2)
 # Space Complexity: O(1)
+class Solution:
+    def largestSquareArea(self, bottomLeft: List[List[int]], topRight: List[List[int]]) -> int: # type: ignore
+        max_size = 0
+
+        for (bottom_left_i,top_right_i),(bottom_left_j,top_right_j) in combinations(zip(bottomLeft,topRight),2): # type: ignore
+            w = min(top_right_i[0],top_right_j[0])-max(bottom_left_i[0],bottom_left_j[0])
+            h = min(top_right_i[1],top_right_j[1])-max(bottom_left_i[1],bottom_left_j[1])
+
+            max_size = max(max_size, min(w,h))
+        return max_size * max_size
