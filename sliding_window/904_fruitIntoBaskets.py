de461ff1bff5a60e0c87e85da36d814af48f14f2
Adii22-22 <adinagalkar@gmail.com>
2026-01-22 21:46:42 +0530

    code uploaded

--- sliding_window/904_fruitIntoBaskets.py
+++ sliding_window/904_fruitIntoBaskets.py
@@ -18,3 +18,23 @@
 #
 # Time Complexity: O(n) — each fruit is added and removed at most once
 # Space Complexity: O(1) — hashmap stores at most 2 keys
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int: # type: ignore
+        fruit_dict = {}
+        p1= 0 
+        win_length = 0
+
+        for p2 in range(len(fruits)):
+            if fruits[p2] in fruit_dict:
+                fruit_dict[fruits[p2]]+=1
+            else:
+                fruit_dict[fruits[p2]]=1
+
+            while len(fruit_dict) > 2:
+                fruit_dict[fruits[p1]] -= 1
+                if fruit_dict[fruits[p1]] == 0:
+                    del fruit_dict[fruits[p1]]
+                p1+=1
+            win = p2-p1+1
+            win_length = max(win_length,win)
+        return win_length
