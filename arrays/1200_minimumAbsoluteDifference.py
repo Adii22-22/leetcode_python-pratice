e698211c224df8d9e66afc9dd7f8bfcd0f9b68e8
Adii22-22 <adinagalkar@gmail.com>
2026-01-26 18:37:58 +0530

    code uploaded

--- arrays/1200_minimumAbsoluteDifference.py
+++ arrays/1200_minimumAbsoluteDifference.py
@@ -16,3 +16,18 @@
 #
 # Time Complexity: O(n log n) due to sorting
 # Space Complexity: O(1) extra space (excluding output list)
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]: # type: ignore
+        arr.sort()
+        pair_list = []
+        min_value = float("inf")
+
+        for i in range(len(arr)-1):
+            min_abs_diff = arr[i+1] - arr[i]
+
+            if min_abs_diff < min_value:
+                min_value = min_abs_diff
+                pair_list = [[arr[i],arr[i+1]]]
+            elif min_value == min_abs_diff:
+                pair_list.append([arr[i],arr[i+1]])
+        return pair_list
