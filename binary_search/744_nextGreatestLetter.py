16c42b50b56fd69dc7f9872aae8aef456127b604
Adii22-22 <adinagalkar@gmail.com>
2026-01-31 21:10:22 +0530

    code uploaded

--- binary_search/744_nextGreatestLetter.py
+++ binary_search/744_nextGreatestLetter.py
@@ -14,3 +14,17 @@
 #
 # Time Complexity: O(log n)
 # Space Complexity: O(1)
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str: # type: ignore
+        l, r = 0, len(letters) - 1
+        ans = letters[0]
+
+        while l <= r:
+            mid = (l + r) // 2
+            if letters[mid] > target:
+                ans = letters[mid]
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return ans
