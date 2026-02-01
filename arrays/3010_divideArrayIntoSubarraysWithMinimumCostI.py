e3a40bd2775006024bb07a2b2387ac1fa37e8909
Adii22-22 <adinagalkar@gmail.com>
2026-02-01 20:43:10 +0530

    comments added

---
+++ arrays/3010_divideArrayIntoSubarraysWithMinimumCostI.py
@@ -0,0 +1,17 @@
+# LeetCode 3010
+# Problem: Divide an Array Into Subarrays With Minimum Cost I
+# Link: https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-i/
+#
+# Approach:
+# The array must be divided into exactly 3 contiguous subarrays.
+# The cost of each subarray is its first element.
+#
+# The first subarray must start at index 0, so nums[0] is always included.
+# To minimize total cost, we only need to choose the starting indices
+# of the 2nd and 3rd subarrays from the remaining elements.
+#
+# Therefore, we pick the two smallest values from nums[1:].
+# Total cost = nums[0] + smallest + second smallest.
+#
+# Time Complexity: O(n log n) due to sorting
+# Space Complexity: O(1) extra space (ignoring input array)
