0907250d607673a91dcd173f540282e8c19d52b1
Adii22-22 <adinagalkar@gmail.com>
2026-02-07 14:10:09 +0530

    code uploaded

--- strings/1653_minimumDeletionsToMakeStringBalanced.py
+++ strings/1653_minimumDeletionsToMakeStringBalanced.py
@@ -23,3 +23,15 @@
 #
 # Time Complexity: O(n)
 # Space Complexity: O(1)
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        b_count = 0
+        deletions = 0
+
+        for ch in s:
+            if ch == 'b':
+                b_count +=1
+            else:
+                if b_count > 0: 
+                    deletions = min(deletions+1,b_count)
+        return deletions     
