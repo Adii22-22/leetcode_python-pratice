1787e3620ea802ddd06b2b42d9fdd38690b830df
Adii22-22 <adinagalkar@gmail.com>
2026-02-15 15:30:51 +0530

    code uploaded

--- math/67_addBinary.py
+++ math/67_addBinary.py
@@ -20,3 +20,32 @@
 #
 # Time Complexity: O(n)
 # Space Complexity: O(n)
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        x,y = int(a,2),int(b,2)
+        z = x+y
+        return format(z,'b')  
+    
+#interview version 
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        i = len(a) - 1
+        j = len(b) - 1
+        carry = 0
+        result = []
+
+        while i >= 0 or j >= 0 or carry:
+            total = carry
+
+            if i >= 0:
+                total += int(a[i])
+                i -= 1
+
+            if j >= 0:
+                total += int(b[j])
+                j -= 1
+
+            result.append(str(total % 2))
+            carry = total // 2
+
+        return ''.join(result[::-1])
