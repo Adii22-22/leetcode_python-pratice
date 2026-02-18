8a079bf98002bd811570924784e739432fd9425e
Adii22-22 <adinagalkar@gmail.com>
2026-02-18 17:15:10 +0530

    code uploaded

--- bit_manipulation/693_hasAlternatingBits.py
+++ bit_manipulation/693_hasAlternatingBits.py
@@ -50,3 +50,7 @@
 # O(1)
 #
 # This is an optimal bit manipulation trick solution.
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        a = (n  ^ (n >> 1))
+        return (a&(a+1)) == 0
